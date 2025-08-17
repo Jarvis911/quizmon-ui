b4a016a88d325162a6e143708245a3c6cb2a4688
Jarvis <triho753@gmail.com>
2025-08-17 19:06:03 +0700

    init react tailwind shadcn

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button.jsx";
+import { LoginForm } from "@/components/login-form.jsx";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <LoginForm></LoginForm>
+    </div>
+  )
+};
+
+export default App;
